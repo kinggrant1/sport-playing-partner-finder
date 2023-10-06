6f2b80357aa7bbf8b9355c662806ae30964c4b56
kinggrant1 <king.grant1@outlook.com>
2023-10-06 09:22:44 -0400

    Added basic content to home page; created tennis, soccer, login/signup pages; added those pages to navbar

---
+++ Pages/LoginSignup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages
+{
+    public class LoginSignupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
