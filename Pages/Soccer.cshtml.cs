c7b812254a7908f03aa92d466af80fec2af54391
Boocherino <dtbucci@coastal.edu>
2023-10-09 11:55:48 -0400

    added a controllers folder, added soccerController, added script functionality for the table so that if drop down position is selected it shows players with associated position, removed hard coded values in html code, added javascript.

--- Pages/Soccer.cshtml.cs
+++ Pages/Soccer.cshtml.cs
@@ -5,9 +5,28 @@ namespace MyWebApp.Pages
 {
     public class SoccerModel : PageModel
     {
-        public string Name {get; set;}
+        public List<Player> Players {get; set;}
+        public SoccerModel()
+        {
+            Players = new List<Player>
+            {
+                new Player {Name = "Ronaldo", Position = "ST", Country = "Portugal"},
+                new Player {Name = "Messi", Position = "ST", Country = "Argentina"},
+                new Player {Name = "Van dijk", Position = "CB", Country = "Netherlands"},
+            };
+        }
         public void OnGet()
         {
         }
+
+        public void AddPlayer(Player p) {
+            Players.Add(p);
+        }
+    }
+    public class Player
+    {
+        public string Name {get; set;}
+        public string Position {get; set;}
+        public string Country {get; set;}
     }
 }
