3f21b639aa11fca033684fbdd86a4a3f1140c9fd
Karolin Krzeminski <kkrzemin@coastal.edu>
2023-10-19 15:17:42 -0400

    Added some functionality to the login/signup page

--- Pages/LoginSignup.cshtml.cs
+++ Pages/LoginSignup.cshtml.cs
@@ -5,8 +5,21 @@ namespace MyWebApp.Pages
 {
     public class LoginSignupModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+    [BindProperty]
+    public string? Username { get; set; }
+
+    [BindProperty]
+    public string? Password { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnPost()
+    {
+        // validate the user credentials and redirect them to a dashboard or show an error.
+
+        return Content($"Username: {Username}, Password: {Password}");  
+    }
     }
 }
